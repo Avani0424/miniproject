ffa3373e4f57e41a31bc1d2ad3647751bdf083d3
Avani0424 <iamavaninambiar@gmail.com>
2025-04-01 23:03:42 +0530

    Update selectscreen.dart

--- selectscreen.dart
+++ selectscreen.dart
@@ -4,188 +4,169 @@ import 'package:miniproject/curriculum.dart';
 import 'package:miniproject/education.dart';
 import 'package:miniproject/inputscreen.dart';
 import 'package:miniproject/skills.dart';
+import 'package:miniproject/templateselectionpage.dart';
 import 'package:miniproject/workdetails.dart';
 
 class Selectscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Get the screen width and height
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFAD9CD0),
-        title: Text(
-          "Resume Sections",
-          style: TextStyle(fontSize: screenWidth * 0.06), // 6% of screen width
+        backgroundColor: const Color(0xFFAD9CD0),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    TemplateSelectionPage(),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  var tween = Tween(begin: 0.0, end: 1.0);
+                  var fadeAnimation = animation.drive(tween);
+                  return FadeTransition(opacity: fadeAnimation, child: child);
+                },
+              ),
+            );
+          },
         ),
-        automaticallyImplyLeading: false,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(screenWidth * 0.04), // 4% of screen width
-        child: Center(
-          child: GridView.count(
-            crossAxisCount: 2,
-            crossAxisSpacing: screenWidth * 0.04, // 4% of screen width
-            mainAxisSpacing: screenHeight * 0.02, // 2% of screen height
-            childAspectRatio: 1.0, // Keeps the aspect ratio square (1:1)
-            children: [
-              SectionBox(
-                title: "Personal Details",
-                imagePath: "assets/images/person.png",
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          Inputscreen(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = 0.0;
-                        var end = 1.0;
-                        var tween = Tween(begin: begin, end: end);
-                        var fadeAnimation = animation.drive(tween);
-
-                        return FadeTransition(
-                          opacity: fadeAnimation,
-                          child: child,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              SectionBox(
-                title: "Qualification",
-                imagePath: "assets/images/qualification.png",
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          Education(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = 0.0;
-                        var end = 1.0;
-                        var tween = Tween(begin: begin, end: end);
-                        var fadeAnimation = animation.drive(tween);
-
-                        return FadeTransition(
-                          opacity: fadeAnimation,
-                          child: child,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              SectionBox(
-                title: "Work History",
-                imagePath: "assets/images/works.png",
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          Workdetails(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = 0.0;
-                        var end = 1.0;
-                        var tween = Tween(begin: begin, end: end);
-                        var fadeAnimation = animation.drive(tween);
-
-                        return FadeTransition(
-                          opacity: fadeAnimation,
-                          child: child,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              SectionBox(
-                title: "Achievements",
-                imagePath: "assets/images/achievement.png",
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          AchievementsPage(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = 0.0;
-                        var end = 1.0;
-                        var tween = Tween(begin: begin, end: end);
-                        var fadeAnimation = animation.drive(tween);
-
-                        return FadeTransition(
-                          opacity: fadeAnimation,
-                          child: child,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              SectionBox(
-                title: "Skills",
-                imagePath: "assets/images/skill.png",
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          Skills(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = 0.0;
-                        var end = 1.0;
-                        var tween = Tween(begin: begin, end: end);
-                        var fadeAnimation = animation.drive(tween);
-
-                        return FadeTransition(
-                          opacity: fadeAnimation,
-                          child: child,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              SectionBox(
-                title: "Curriculum Vitae",
-                imagePath: "assets/images/curriculum.png",
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          Curriculum(),
-                      transitionsBuilder:
-                          (context, animation, secondaryAnimation, child) {
-                        var begin = 0.0;
-                        var end = 1.0;
-                        var tween = Tween(begin: begin, end: end);
-                        var fadeAnimation = animation.drive(tween);
-
-                        return FadeTransition(
-                          opacity: fadeAnimation,
-                          child: child,
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-            ],
+        title: Text(
+          'Resume Sections',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: screenWidth * 0.050,
           ),
         ),
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(screenWidth * 0.04), // 4% of screen width
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: screenWidth * 0.04, // 4% of screen width
+                mainAxisSpacing: screenHeight * 0.02, // 2% of screen height
+                childAspectRatio: 1.0, // Keeps the aspect ratio square (1:1)
+                children: [
+                  SectionBox(
+                    title: "Personal Details",
+                    imagePath: "assets/images/person.png",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Inputscreen()),
+                      );
+                    },
+                  ),
+                  SectionBox(
+                    title: "Qualification",
+                    imagePath: "assets/images/qualification.png",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Education()),
+                      );
+                    },
+                  ),
+                  SectionBox(
+                    title: "Work History",
+                    imagePath: "assets/images/works.png",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Workdetails()),
+                      );
+                    },
+                  ),
+                  SectionBox(
+                    title: "Achievements",
+                    imagePath: "assets/images/achievement.png",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AchievementsPage()),
+                      );
+                    },
+                  ),
+                  SectionBox(
+                    title: "Skills",
+                    imagePath: "assets/images/skill.png",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Skills()),
+                      );
+                    },
+                  ),
+                  SectionBox(
+                    title: "Curriculum Vitae",
+                    imagePath: "assets/images/curriculum.png",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Curriculum()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // Generate PDF Button at Bottom
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: screenHeight * 0.02),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 234, 196, 235),
+                    Color.fromARGB(255, 114, 37, 196),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  generatePDF();
+                },
+                icon: Icon(Icons.picture_as_pdf, color: Colors.white),
+                label: Text(
+                  "Generate PDF",
+                  style: TextStyle(
+                      fontSize: screenWidth * 0.045, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.transparent, // Transparent to show gradient
+                  shadowColor: Colors.transparent, // Remove button shadow
+                  padding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.02,
+                      horizontal: screenWidth * 0.1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void generatePDF() {
+    // TODO: Implement PDF generation logic
+    print("Generating PDF...");
   }
 }
 
@@ -202,7 +183,7 @@ class SectionBox extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return GestureDetector(
-      onTap: onTap, // Detect tap gesture
+      onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -219,18 +200,17 @@ class SectionBox extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // Image widget above the title
             Image.asset(
               imagePath,
-              width: screenWidth * 0.18, // 18% of screen width
-              height: screenWidth * 0.18, // 18% of screen width (square image)
+              width: screenWidth * 0.18,
+              height: screenWidth * 0.18,
             ),
-            SizedBox(height: 8), // Space between image and text
+            SizedBox(height: 8),
             Text(
               title,
               style: TextStyle(
                 color: Colors.white,
-                fontSize: screenWidth * 0.05, // 5% of screen width
+                fontSize: screenWidth * 0.05,
                 fontWeight: FontWeight.bold,
               ),
             ),
